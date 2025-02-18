00b65209428aaa65c333614b573300da28262362
Barbara Ferreira <barbaraferreira12@gmail.com>
2025-02-18 09:43:06 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="space-y-3 h-screen">
+      <main className="p-3">
+        <div className="rounded-mobile md:rounded-desktop bg-grey-100 p-6 shadow-elevation-01 w-full"></div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
